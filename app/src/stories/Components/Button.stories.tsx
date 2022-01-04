badc5bbcb9891fd0b322d525873c68fd0aa70071
Soonki <alstnsrl98@gmail.com>
2022-01-04 14:25:07 +0900

    Feat: Storybook 파일 생성

---
+++ app/src/stories/Components/Button.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Story } from '@storybook/react/types-6-0';
+import { StoryWrapper } from '@components/Common';
+import Button from '@components/Button';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+  // decorators: [(Story: Story) => <Provider store={store}><Story /></Provider>]
+};
+
+const Template: Story = ({ children }) => {
+  return (
+    <StoryWrapper>
+      <Button>{children}</Button>
+    </StoryWrapper>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  children: '편안한',
+};
+
+export const Example = Template.bind({});
+Example.args = {
+  children: '가치가요',
+};
