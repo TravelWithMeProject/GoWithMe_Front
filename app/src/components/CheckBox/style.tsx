a409fb890c8b45b54b90fec25a13afa11c6c1df0
Soonki <alstnsrl98@gmail.com>
2022-01-05 13:57:50 +0900

    Style: modify width, height, font-size, margin Style

--- app/src/components/CheckBox/style.tsx
+++ app/src/components/CheckBox/style.tsx
@@ -9,6 +9,8 @@ export const InvisibleCheckBox = styled.input.attrs({
 export const StyledCheckBoxLabel = styled.label`
   display: flex;
   align-items: center;
+  color: #747474;
+  font-size: 1.5em;
   cursor: pointer;
 `;
 
@@ -16,8 +18,9 @@ export const VisibleCheckBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 20px;
-  height: 20px;
-  border: 1px solid black;
+  width: 25px;
+  height: 25px;
+  border: 1px solid #747474;
   border-radius: 50%;
+  margin-right: 12px;
 `;
