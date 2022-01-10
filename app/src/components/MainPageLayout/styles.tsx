cea3b36a418635280b8bc57c07c4bf43b42ad07a
Soonki <alstnsrl98@gmail.com>
2022-01-10 17:48:26 +0900

    Feat: feature scroll of identityLayout #25

--- app/src/components/MainPageLayout/styles.tsx
+++ app/src/components/MainPageLayout/styles.tsx
@@ -12,6 +12,16 @@ export const MainLayout = styled.div`
   height: 100%;
   overflow: auto;
   background-color: #b9b9b9;
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: #384457;
+    border-radius: 10px;
+  }
+  &::-webkit-scrollbar-track {
+    background-color: rgba(0, 0, 0, 0);
+  }
 `;
 
 export const LoginLayout = styled.div`
