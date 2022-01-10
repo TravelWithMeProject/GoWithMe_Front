9ca18b1dc86788c09a819d82f9fda16a0b09543d
cbkpar <cbkpar@naver.com>
2022-01-10 19:52:13 +0900

    Feat: LoginForm 컴포넌트 생성 #27

---
+++ app/src/components/LoginForm/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyledLoginForm, LoginFormContainer } from './style';
+
+const LoginForm = () => {
+
+  return (
+    <StyledLoginForm>
+      <LoginFormContainer>
+      </LoginFormContainer>
+    </StyledLoginForm>
+  );
+};
+
+export default LoginForm;
---
+++ app/src/components/LoginForm/style.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledLoginForm = styled.div`
+  width: 100%;
+`;
+
+export const LoginFormContainer = styled.div`
+  width: 100%;
+`;
