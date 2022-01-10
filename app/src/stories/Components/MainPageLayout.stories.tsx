4c9d7ad976688737177058e72a6ed514d49ea938
Soonki <alstnsrl98@gmail.com>
2022-01-10 17:49:37 +0900

    Test: add test case #25

--- app/src/stories/Components/MainPageLayout.stories.tsx
+++ app/src/stories/Components/MainPageLayout.stories.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { Story } from '@storybook/react/types-6-0';
 import { StoryWrapper } from '@components/Common';
 import MainPageLayout from '@components/MainPageLayout';
+import styled from 'styled-components';
 
 export default {
   title: 'Components/MainPageLayout',
   component: MainPageLayout,
 };
 
+const Identity = styled.div`
+  width: 100%;
+  height: 2000px;
+  background-color: #436abe;
+`;
+
+const Example = () => {
+  return <Identity />;
+};
 const Template: Story = ({ identity, form }) => {
   return (
     <StoryWrapper>
@@ -18,6 +28,6 @@ const Template: Story = ({ identity, form }) => {
 
 export const Default = Template.bind({});
 Default.args = {
-  identity: <div>identity</div>,
+  identity: <Example />,
   form: <div>form</div>,
 };
