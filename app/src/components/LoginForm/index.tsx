d555ff460ed7bc03cdf6332ca19949beddd438de
cbkpar <cbkpar@naver.com>
2022-01-11 12:51:00 +0900

    Feat: LoginForm 레이아웃 제작 #27

--- app/src/components/LoginForm/index.tsx
+++ app/src/components/LoginForm/index.tsx
@@ -1,36 +1,45 @@
 import Button from '@components/Button';
 import CheckBox from '@components/CheckBox';
 import React from 'react';
-import { StyledLoginForm, LoginFormContainer } from './style';
+import { StyledLoginForm, LoginFormContainer, InputWrap, KeepLoginWrap, FindWrap, ButtonWrap, OtherLoginWrap } from './style';
 
 const LoginForm = () => {
 
   return (
     <StyledLoginForm>
       <LoginFormContainer>
-        <input></input>
-        <input></input>
-        <CheckBox value="1" onChange={() => { }} key="1">
-          자동 로그인
-        </CheckBox>
-        아이디 / 비밀번호 찾기
-        <Button
-          border="round"
-          onClick={() => { }}
-          width="70%"
-          size="large"
-        >로그인
-        </Button>
-        <Button
-          varient="outlined"
-          border="round"
-          onClick={() => { }}
-          width="70%"
-          size="large"
-        >회원가입
-        </Button>
-        또는 SNS로 간편하게 로그인/회원가입
-
+        <InputWrap>
+          <input></input>
+          <input></input>
+        </InputWrap>
+        <KeepLoginWrap>
+          <CheckBox value="1" onChange={() => { }} key="1">
+            자동 로그인
+          </CheckBox>
+        </KeepLoginWrap>
+        <FindWrap>
+          아이디 / 비밀번호 찾기
+        </FindWrap>
+        <ButtonWrap>
+          <Button
+            border="round"
+            onClick={() => { }}
+            width="100%"
+            size="large"
+          >로그인
+          </Button>
+          <Button
+            varient="outlined"
+            border="round"
+            onClick={() => { }}
+            width="100%"
+            size="large"
+          >회원가입
+          </Button>
+        </ButtonWrap>
+        <OtherLoginWrap>
+          또는 SNS로 간편하게 로그인 / 회원가입
+        </OtherLoginWrap>
       </LoginFormContainer>
     </StyledLoginForm>
   );
--- app/src/components/LoginForm/style.tsx
+++ app/src/components/LoginForm/style.tsx
@@ -1,9 +1,50 @@
 import styled from 'styled-components';
 
-export const StyledLoginForm = styled.div`
+export const StyledLoginForm = styled.form`
   width: 100%;
 `;
 
-export const LoginFormContainer = styled.form`
+export const LoginFormContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
   width: 100%;
+  padding: 10px;
+  margin: 0 20px;
+`;
+
+export const InputWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 10px 0;
+  width: 100%;
+
+  input {
+    margin: 5px;
+  }
+`;
+
+export const KeepLoginWrap = styled.div`
+  margin: 10px 0;
+  width: 50%;
+`;
+
+export const FindWrap = styled.div`
+  margin: 10px 0;
+  width: 50%;
+  text-align: right;
+`;
+
+export const ButtonWrap = styled.div`
+  margin: 10px 0;
+  width: 100%;
+  text-align: center;
+  > button {
+    margin: 5px 0;
+  }
+`;
+
+export const OtherLoginWrap = styled.div`
+  margin: 10px 0;
+  width: 100%;
+  text-align: center;
 `;
