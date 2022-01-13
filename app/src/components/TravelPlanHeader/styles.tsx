7ac9a97cb9de5abd944f88570e4dc0b237e28a97
Min <alstnsrl98@gmail.com>
2022-01-13 22:15:51 +0900

    Style: 와이어 프레임 작성 #36

--- app/src/components/TravelPlanHeader/styles.tsx
+++ app/src/components/TravelPlanHeader/styles.tsx
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
-export const StyledTravelPlanHeader = styled.header``;
+export const StyledTravelPlanHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 200px;
+  border: 1px solid;
+`;
