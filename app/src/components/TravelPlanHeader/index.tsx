0deb9695f95be2abc57eb395ca2793b1d8380334
Min <alstnsrl98@gmail.com>
2022-01-14 00:07:10 +0900

    Feat: TravelInfoWrapper 생성 #36

--- app/src/components/TravelPlanHeader/index.tsx
+++ app/src/components/TravelPlanHeader/index.tsx
@@ -1,4 +1,9 @@
-import { StyledTravelPlanHeader, TitleWrapper } from './styles';
+import Button from '@components/Button';
+import {
+  StyledTravelPlanHeader,
+  TitleWrapper,
+  TravelInfoWrapper,
+} from './styles';
 
 interface Props {
   title?: string;
@@ -11,6 +16,33 @@ const TravelPlanHeader = ({ title = '내 여행 일정 이름' }: Props) => {
         <h1>{title}</h1>
         <h3>두근두근... 출발까지 D-00</h3>
       </TitleWrapper>
+      <TravelInfoWrapper>
+        <ul>
+          <li>
+            <span>여행지 | 부산 제주</span>
+          </li>
+          <li>
+            <span>여행 날짜 | 2022.01.01 - 2022.01.10</span>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Button border="round" size="small" onClick={() => {}}>
+              수정
+            </Button>
+          </li>
+          <li>
+            <Button border="round" size="small" onClick={() => {}}>
+              복제
+            </Button>
+          </li>
+          <li>
+            <Button border="round" size="small" onClick={() => {}}>
+              삭제
+            </Button>
+          </li>
+        </ul>
+      </TravelInfoWrapper>
     </StyledTravelPlanHeader>
   );
 };
--- app/src/components/TravelPlanHeader/styles.tsx
+++ app/src/components/TravelPlanHeader/styles.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const StyledTravelPlanHeader = styled.header`
   display: flex;
   flex-direction: column;
+  justify-content: space-around;
   width: 100%;
   height: 200px;
   padding: 0 102px;
@@ -13,11 +14,27 @@ export const TitleWrapper = styled.div`
   display: flex;
   flex-direction: column;
   h1 {
-    font-size: 48px;
+    font-size: 3em;
     color: #5a87ff;
   }
   h3 {
-    font-size: 24px;
+    font-size: 1.5em;
     color: #5a86ff6e;
+  }
+`;
+
+export const TravelInfoWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  ul {
+    display: flex;
+    li {
+      margin-right: 27px;
+    }
+  }
+  button {
+    display: flex;
+    align-items: center;
+    padding: 8px 14px;
   }
 `;
