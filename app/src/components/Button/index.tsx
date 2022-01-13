d76b680ceaf7a47895043e26da9aa32b3d1ef8c0
Min <alstnsrl98@gmail.com>
2022-01-13 21:48:18 +0900

    Fix: button onClick props를 optional로 변경 #34

--- app/src/components/Button/index.tsx
+++ app/src/components/Button/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyledButton } from './style';
 
 interface Props {
-  onClick: (e?: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick?: (e?: React.MouseEvent<HTMLButtonElement>) => void;
   children: React.ReactNode;
   width?: string;
   border?: 'round' | 'rect';
@@ -11,7 +11,7 @@ interface Props {
 }
 
 const Button = ({
-  onClick,
+  onClick = () => {},
   children,
   width,
   border = 'rect',
