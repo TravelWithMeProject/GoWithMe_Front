fc2c80e2d472a9c968e6685b56edd5c1572299bc
Min <alstnsrl98@gmail.com>
2022-01-16 21:47:45 +0900

    Fix: style 수정 방식 useMemo에서 className으로 변경 #34

--- app/src/components/Button/index.tsx
+++ app/src/components/Button/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { StyledButton } from './style';
 
 interface Props {
@@ -8,7 +8,7 @@ interface Props {
   border?: 'round' | 'rect';
   varient?: 'contained' | 'outlined';
   size?: 'large' | 'medium' | 'small';
-  onClick?: (e?: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const Button = ({
@@ -18,50 +18,15 @@ const Button = ({
   border = 'rect',
   varient = 'contained',
   size = 'large',
-  onClick = () => {},
+  onClick,
 }: Props) => {
-  const buttonHeight = useMemo(() => {
-    switch (size) {
-      case 'large':
-        return '48px';
-      case 'medium':
-        return '36px';
-      case 'small':
-        return '24px';
-    }
-  }, [size]);
-
-  const buttonPadding = useMemo(() => {
-    switch (size) {
-      case 'large':
-        return '0px 20px';
-      case 'medium':
-        return '0px 16px';
-      case 'small':
-        return '0px 12px';
-    }
-  }, [size]);
-
-  const fontSize = useMemo(() => {
-    switch (size) {
-      case 'large':
-        return '1em';
-      case 'medium':
-        return '0.875em';
-      case 'small':
-        return '0.875em';
-    }
-  }, [size]);
-
   return (
     <StyledButton
-      onClick={(e) => onClick(e)}
+      className={size}
+      onClick={onClick}
       varient={varient}
       border={border}
       fill={fill}
-      buttonHeight={buttonHeight}
-      buttonPadding={buttonPadding}
-      fontSize={fontSize}
       disabled={disabled}
     >
       {children}
--- app/src/components/Button/style.tsx
+++ app/src/components/Button/style.tsx
@@ -4,26 +4,35 @@ export const StyledButton = styled.button<{
   varient: string;
   border: string;
   fill: boolean;
-  buttonHeight: string;
-  buttonPadding: string;
-  fontSize: string;
   disabled: boolean;
 }>`
   border: 1px solid #5a87ff;
   transition: 0.5s;
   width: ${(props) => (props.fill ? '100%' : 'auto')};
-  height: ${(props) => props.buttonHeight};
   border-radius: ${(props) => (props.border === 'round' ? 6 : 0)}px;
   background-color: ${(props) =>
     props.varient === 'contained' ? '#5A87FF' : '#fff'};
   color: ${(props) => (props.varient === 'contained' ? '#fff' : '#5A87FF')};
-  padding: ${(props) => (props.fill ? 0 : props.buttonPadding)};
-  font-size: ${(props) => props.fontSize};
   cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
   &:hover {
     filter: ${(props) => !props.disabled && 'brightness(90%)'};
   }
   &:active {
     filter: brightness(100%);
+  }
+  &.large {
+    height: 48px;
+    padding: 0px 20px;
+    font-size: 1rem;
+  }
+  &.medium {
+    height: 36px;
+    padding: 0px 16px;
+    font-size: 0.875rem;
+  }
+  &.small {
+    height: 24px;
+    padding: 0px 12px;
+    font-size: 0.875rem;
   }
 `;
